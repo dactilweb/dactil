88da0533f7c2d27d477c6d0333681366a7e2d7f4
Manu <10000189.joan23@fje.edu>
2019-05-02 20:49:35 +0200

    push

--- dactilWeb/src/java/model/FacturaDAO.java
+++ dactilWeb/src/java/model/FacturaDAO.java
@@ -25,7 +25,7 @@ public class FacturaDAO {
     private Conexion con = new Conexion();
     private Connection cn = con.conectar();
     private String sql;
-
+    private String data;
     public FacturaDAO() {
     }
 
@@ -43,7 +43,7 @@ public class FacturaDAO {
                 Factura factura = new Factura();
                 factura.setNombre_producto(rs.getString("nombre_producto"));
                 factura.setPrecio_producto(rs.getInt("precio_producto"));
-                factura.setFecha_factura(rs.getString("fecha_factura"));
+                JOptionPane.showMessageDialog(null,rs.getString("fecha_factura"));
                 factura.setCantidad_compra(rs.getInt("cantidad_compra"));
                 factura.setNombre_cliente(rs.getString("nombre_cliente"));
                 factura.setApellido_cliente(rs.getString("apellido_cliente"));
